a1062312b8bd235339f18bb7c938fcfe543932b7
Nahim Udoy <Nahimbinrezaudoy@gmail.com>
2021-10-04 08:06:30 +0600

    added app layout & footer partial

---
+++ resources/views/frontend/layout/app.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name')}}</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+        <link href="{{ mix('css/all.css') }}" rel="stylesheet">
+        @yield('before_header')
+
+    </head>
+    <body class="antialiased">
+        @include('frontend.partials._header')
+          
+          <main>
+          
+            @yield('content')
+          
+          </main>
+          
+          @include('frontend.partials._footer')
+          
+
+        
+        <script src="{{ mix('js/all.js') }}">
+        @yield('before_body')
+    </body>
+</html>
--- resources/views/frontend/partials/_footer.blade.php
+++ resources/views/frontend/partials/_footer.blade.php
@@ -1,9 +1,9 @@
-<footer class="text-muted py-5">
+<footer class="text-muted py-5 text-center">
     <div class="container">
       <p class="float-end mb-1">
         <a href="#">Back to top</a>
       </p>
-      <p class="mb-1">Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-      <p class="mb-0">New to Bootstrap? <a href="/">Visit the homepage</a> or read our <a href="/docs/5.1/getting-started/introduction/">getting started guide</a>.</p>
+      <p class="mb-1">&copy; {{ config('app.name') }} {{ date('Y') }}</p>
+      <p class="mb-0"><a href="#">Provacy Policy</a> or <a href="#">Contact Us</a>.</p>
     </div>
   </footer>
